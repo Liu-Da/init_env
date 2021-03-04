cbe932a63df1d80c8321054524bf0937ec41f1f9
Liu-Da <liuda0112@gmail.com>
2021-03-04 17:01:45 +0800

    pure theme setting

--- init_zim.sh
+++ init_zim.sh
@@ -7,7 +7,7 @@ git clone https://github.com/skywind3000/z.lua.git $HOME/.z.lua
 grep -w "z.lua --init zsh" $HOME/.zshrc >/dev/null || echo 'eval "$(lua $HOME/.z.lua/z.lua --init zsh)"' >> $HOME/.zshrc
 
 # pure
-grep -w "zmodule sindresorhus/pure" $HOME/.zimrc >/dev/null || echo 'zmodule sindresorhus/pure --source async.zsh --source pure.zsh' >> $HOME/.zimrc
+sed -i 's#zmodule steeef#zmodule sindresorhus/pure --source async.zsh --source pure.zsh#g' ~/.zimrc
 source $HOME/.zshrc
 zimfw install
 
